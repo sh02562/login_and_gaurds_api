7bcafa78decc2a475a80274de50e29223e321b54
sh02562 <shweta02562@gmail.com>
2024-04-04 13:05:13 +0530

    Auth api

---
+++ AngularAuthApi/Context/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using AngularAuthApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthApi.Context
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) 
+        {
+        
+        
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");
+        }
+    }
+}
---
+++ AngularAuthApi/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using AngularAuthApi.Context;
+using AngularAuthApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularAuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDBContext _authDbContext;
+        public UserController(AppDBContext authDbContext)
+        {
+            this._authDbContext = authDbContext;
+        }
+
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            var user = await _authDbContext.Users.FirstOrDefaultAsync(x=>x.Email == userObj.Email && x.Password == userObj.Password);
+            if (user == null)
+                return NotFound(new { Message = "User Not Found" });
+
+            return Ok(new { Message = "Login Success!" });
+        }
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> RegisterUser([FromBody] User userObj)
+        {
+            if (userObj == null)
+                return BadRequest();
+
+            await _authDbContext.Users.AddAsync(userObj);
+            await _authDbContext.SaveChangesAsync();
+            return Ok(new { Message = "User Registered!" });
+
+        }
+    }
+}
